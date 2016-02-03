8e9b67cee0649cb02d2a66b3ca112d6d70bc200d
Simon Bloom <swbloom@gmail.com>
2016-02-02 19:03:06 -0500

    Added template screenshot, removed bower dependencies, added Picasso Pro specific stylesheet content, added social-icons component,  added media queries and colour variables, added css reset, removed some bootstrap specific styles, added footer styles, created footer plugin

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,34 @@
+<?php
+  function display_social_media_icons() {
+    $facebook = get_option('facebook') ?
+      "<a href='http://www.facebook.com/" . get_option('facebook') . "'> <span class='social-icon facebook'>F</a></span>"
+      : null;
+    $twitter = get_option('twitter') ?
+      "<a href='http://www.twitter.com/" . get_option('twitter') . "'> <span class='social-icon twitter'>T</a></span>"
+      : null;;
+    $youtube = get_option('vimeo') ?
+      "<a href='http://www.vimeo.com/" . get_option('vimeo') . "'> <span class='social-icon vimeo'>V</a></span>"
+      : null;
+
+?>
+
+    <?php
+      echo $facebook;
+      echo $twitter;
+      echo $youtube;
+    ?>
+    </div>
+<?php
+  }
+?>
+
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+    <div class="container">
+      <div class="copyright">
+        &copy; <?php echo date('Y'); ?> <?php echo get_bloginfo('name') ?> <?php echo strtolower(get_bloginfo('description')) ?>
+      </div>
+      <div class="social">
+        <?php display_social_media_icons() ?>
+      </div>
+    </div>
 </footer>
