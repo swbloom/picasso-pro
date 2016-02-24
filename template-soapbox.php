93308226433993bdb9b3ec9dcfad1504dc8a936e
Simon Bloom <swbloom@gmail.com>
2016-02-23 20:56:19 -0500

    Added articles and layout to the soapbox page

--- template-soapbox.php
+++ template-soapbox.php
@@ -21,41 +21,44 @@
   </div>
 </div>
 
-<!-- Going back into the container -->
-<div class="wrap container">
-  <div class="content row">
-    <div class="main">
-      <?php
-        $resourcesQuery = new WP_QUERY(
-          array(
-            'post_type' => 'resource',
-            'order' => 'ASC'
-            )
-      ); ?>
+<!-- Small hack, going back into the container -->
+<span>
+  <div class="wrap container">
+    <div class="content row">
+      <div class="main">
+        <?php
+          $soapboxQuery = new WP_QUERY(
+            array(
+              'post_type' => 'soapbox-article',
+              'order' => 'ASC'
+              )
+        ); ?>
 
-      <hr class="rule rule--thick" />
-      <!-- <section class="resources-wrapper">
-      <?php if ( $resourcesQuery->have_posts() ) : ?>
-        <?php while ( $resourcesQuery->have_posts()) : $resourcesQuery->the_post(); ?>
-          <article class="resource" id="<?php echo $post->post_name; ?>">
-            <p class="resource-category"><?php the_field('resource-category'); ?></p>
-            <h2><?php the_title(); ?></h2>
-            <p class="resource-dop"><?php the_field('resource-dop'); ?></p>
-            <div class="resource-summary">
-              <?php the_field('resource-summary');  ?>
-            </div>
-            <?php $resource = get_field('resource') ?>
-            <a class="resource-download" download="" href="<?php echo $resource['url'] ?>">Download PDF</a>
-          </article>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-      <?php endif; ?>
-      </section> -->
-      <!-- <?php if (!have_posts()) : ?>
-        <div class="alert alert-warning">
-          <?php _e('Sorry, no results were found.', 'sage'); ?>
-        </div>
-        <?php get_search_form(); ?>
-      <?php endif; ?> -->
+        <section class="soapbox-wrapper">
+        <?php if ( $soapboxQuery->have_posts() ) : ?>
+          <?php while ( $soapboxQuery->have_posts()) : $soapboxQuery->the_post(); ?>
+            <article id="<?php echo $post->post_name; ?>">
+              <h2 class="article-title"><?php the_title(); ?></h2>
+              <p class="article-date"><?php the_date(); ?></p>
+              <div class="article-featured-image">
+                <?php $article_feature_image = get_field('article-feature-image'); ?>
+                <img src="<?php echo $article_feature_image['url'] ?>" alt="<?php echo $article_feature_image['alt'] ?>" ?>
+              </div>
+              <div class="article-excerpt">
+                <?php the_field('article-excerpt') ?>
+              </div>
+              <a class="permalink" href="<?php the_permalink() ?>">Read More</a>
+            </article>
+            <hr class="rule rule--thick">
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?>
+        <?php endif; ?>
+        </section>
+        <!-- <?php if (!have_posts()) : ?>
+          <div class="alert alert-warning">
+            <?php _e('Sorry, no results were found.', 'sage'); ?>
+          </div>
+          <?php get_search_form(); ?>
+        <?php endif; ?> -->
     </div>
-
+  </span>
