94ca9e4848dfb184f9eff672e4d123dc6a9c8696
Simon Bloom <swbloom@gmail.com>
2016-03-20 17:43:02 -0400

    Style the search bar

--- searchform.php
+++ searchform.php
@@ -5,7 +5,7 @@
 ?>
 <form role="search" method="get" id="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <div class="search-wrap">
-      <label class="screen-reader-text" for="search-input"><?php _e( 'Search for:', 'presentation' ); ?></label>
+      <label for="search-input"><img src="<?php echo get_template_directory_uri() ?>/assets/images/magnifyingglass.png" alt="Click here to search" class="magnifying-glass"></label>
         <input type="search" placeholder="<?php echo esc_attr( 'Search the Site', 'presentation' ); ?>" name="s" id="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" />
         <input class="screen-reader-text" type="submit" id="search-submit" value="Search" />
     </div>
